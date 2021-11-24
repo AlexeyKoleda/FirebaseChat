11b95cc371c254f46ad921e3f8256c26d938cfb9
Alexey Koleda <alexeykoleda@icloud.com>
2021-11-24 10:54:30 +0300

    Initial Commit

---
+++ FirebaseChat/FirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseChatApp.swift
+//  FirebaseChat
+//
+//  Created by Alexey Koleda on 24.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
